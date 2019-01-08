a008277c6e54996d19d38467131bdd8fc2970b5a
bmkwak22 <bmkwak22@gmail.com>
2019-01-08 10:38:49 +0900

    no message

--- java-workspace/java/src/com/bit/day02/IFMain.java
+++ java-workspace/java/src/com/bit/day02/IFMain.java
@@ -1,14 +1,41 @@
 package com.bit.day02;
 
+import java.util.Scanner;
+
 public class IFMain {
 	
 	public static void main(String[] args) {
 		int money = 2000;
-		if(money ==2300) {
+		if(money >= 2300) {
 			System.out.println("커피를 사먹을 수 있어!");
 		}else {
 			System.out.println("물이나 마셔야지...");
 		}
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("보유 금액 입력 : ");
+		money = s.nextInt();
+		if(money >= 10000) {
+			System.out.println("너 돈 좀 있네?");
+		}else {
+			System.out.println("용돈 좀 줄까?");
+		}
+		System.out.println("입력한 금액 : " + money);
+		
+		int num = 10;
+		System.out.println("입력된 수는 ");
+		if(num < 0) {
+			System.out.println("0미만");
+		}else {
+			System.out.println("0이상");
+			if(num<100) {
+				System.out.println("0이상 100미만");
+			}else {
+				System.out.println("100이상");		
+			}
+		}
+		
+		s.close();
 	}
 	
 }
--- java-workspace/java/src/com/bit/day02/ScannerMain.java
+++ java-workspace/java/src/com/bit/day02/ScannerMain.java
@@ -13,6 +13,7 @@ public class ScannerMain {
 		
 		System.out.print("실수 입력 : ");
 		double dNum = s.nextDouble();
+		s.nextLine(); // 입력 버퍼를 비우기 위해 사용
 		System.out.println("실수 : " + dNum);
 		System.out.println();
 		
@@ -23,7 +24,20 @@ public class ScannerMain {
 		
 		System.out.print("문자열 입력 : ");
 		String str = s.next();
-		System.out.println("문자열 : str");
+		s.nextLine();
+		System.out.println("문자열 : " + str);
+		System.out.println();
+		
+		System.out.print("문장 입력 : ");
+		String str2 = s.nextLine();
+		System.out.println("문자열 : " + str2);
+		System.out.println();
+		
+		System.out.print("논리값 입력 : ");
+		boolean bool = s.nextBoolean();
+		System.out.println("논리값 : " + bool);
+		
+		s.close();
 	}
 	
 }
