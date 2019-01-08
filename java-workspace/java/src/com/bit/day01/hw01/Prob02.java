45700e864ee50d8db60a1f891dd72134703691d7
bmkwak22 <bmkwak22@gmail.com>
2019-01-08 09:28:45 +0900

    no message

---
+++ java-workspace/java/src/com/bit/day01/hw01/Prob02.java
@@ -0,0 +1,14 @@
+package com.bit.day01.hw01;
+
+public class Prob02 {
+	
+	public static void main(String[] args) {
+		for(int i=1; i<9; i++) {
+			for(int j=0; j<10; j++) {
+				System.out.print(i+j+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+}
---
+++ java-workspace/java/src/com/bit/day02/WhatMain.java
@@ -0,0 +1,9 @@
+package com.bit.day02;
+
+public class WhatMain {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
